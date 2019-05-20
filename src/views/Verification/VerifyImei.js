7a5d489980d5e8a8a0be8a7fd4b590a00536b97d
Ubaid Rajputt <ubaidrajputt@gmail.com>
2019-05-20 10:51:49 +0500

    Fix regex for Imie

--- src/views/Verification/VerifyImei.js
+++ src/views/Verification/VerifyImei.js
@@ -74,7 +74,7 @@ const MyEnhancedForm = withFormik({
     // IMEIs Validation
     if (!values.imei) {
       errors.imei = 'This field is Required'
-    } else if (!/^(?=.[a-fA-F]*)(?=.[0-9]*)[a-fA-F0-9]+$/.test(values.imei)) {
+    } else if (!/^(?=.[A-F]*)(?=.[0-9]*)[A-F0-9]{14,16}$/.test(values.imei) ) {
       errors.imei = 'IMEI must contains 14 to 16 characters and contains a combination of [0-9], [a-f] and [A-F]'
     }
     return errors;
