894e1230e7b889fc9c4be9d489e16cea7965ddf0
Ubaid Rajputt <ubaidrajputt@gmail.com>
2019-05-24 13:02:21 +0500

    Include parameters for bulk api

--- src/views/BulkVerification/BulkVerify.js
+++ src/views/BulkVerification/BulkVerify.js
@@ -322,6 +322,9 @@ class BulkVerify extends Component {
     })
     const formData = new FormData()
     formData.append('tac', tac)
+    formData.append('username', this.props.userDetails.preferred_username)
+    formData.append('user_id', this.props.userDetails.sub)
+
     this.setState({
       count: JSON.parse(localStorage.getItem('count')),
       trackingIds: JSON.parse(localStorage.getItem('tracking_ids'))
@@ -435,6 +438,7 @@ class BulkVerify extends Component {
   }
 
   componentDidMount() {
+    console.log(this.props);
     this.setState({
       count: JSON.parse(localStorage.getItem('count'))
     }, () => {
