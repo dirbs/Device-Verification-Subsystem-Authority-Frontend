f60d1ad66f91728fa024c3b7507b3c9393d1361f
Ubaid Rajputt <ubaidrajputt@gmail.com>
2019-06-13 15:22:28 +0500

    Introduce Header language

--- src/components/Header/HeaderDropdown.js
+++ src/components/Header/HeaderDropdown.js
@@ -25,6 +25,7 @@ import {
   Dropdown
 } from 'reactstrap';
 import i18next from "i18next";
+import i18n from '../../i18n'
 
 class HeaderDropdown extends Component {
 
@@ -51,7 +52,7 @@ class HeaderDropdown extends Component {
           <span className="fa fa-caret-down"></span>
         </DropdownToggle>
         <DropdownMenu right>
-          <DropdownItem onClick={this.props.kc.logout}><i className="fa fa-lock"></i>{i18next.t("logout")}</DropdownItem>
+          <DropdownItem onClick={this.props.kc.logout}><i className="fa fa-lock"></i>{i18n.t("logout")}</DropdownItem>
         </DropdownMenu>
       </Dropdown>
     );
